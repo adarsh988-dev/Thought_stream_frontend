9d8acebeff2a668ef0a3cdeb088d399605aa1686
Ashu-Thakur <asthakur@bestpeers.com>
2024-12-27 15:52:07 +0530

    push the api changes

--- SERVER/main_thought_stream/main_thought_stream/settings.py
+++ SERVER/main_thought_stream/main_thought_stream/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-tazgseud5u1+36%z4uc4j9l(!1eyt+m1q3pwbvm1-lhi3xdyyz
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'rest_framework',
     'rest_framework.authtoken', 
     'user',
+    'post',
 ]
 
 SIMPLE_JWT = {
@@ -152,6 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- SERVER/main_thought_stream/post/serializers.py
+++ SERVER/main_thought_stream/post/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from .models import Post, Comment
 
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Comment
+        fields = ['id', 'user', 'content', 'created_at']
+
+        
 class PostSerializer(serializers.ModelSerializer):
+    comments = CommentSerializer(many=True, read_only=True)
     class Meta:
         model = Post
         fields = ['id', 'title', 'content', 'image','created_at', 'updated_at']
@@ -11,8 +18,17 @@ class PostSerializer(serializers.ModelSerializer):
         user = self.context['request'].user  
         validate_data['author'] = user  
         return super().create(validate_data)
+    
+    def update(self, instance, validated_data):
+        # Ensure the 'author' is not updated
+        validated_data.pop('author', None)  
 
-class CommentSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Comment
+        # Update other fields
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
+        
 
